f28bb5452c93971c2b4d42e4939e0e9a43230d17
Iwao SUZUKI <i_suzuki@zappallas.com>
2014-12-15 17:34:11 +0900

    Initial Commit

---
+++ Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sample
+//
+//  Created by i_suzuki on 2014/12/15.
+//  Copyright (c) 2014年 i_suzuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sample
+//
+//  Created by i_suzuki on 2014/12/15.
+//  Copyright (c) 2014年 i_suzuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
